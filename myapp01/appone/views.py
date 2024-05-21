611b57cf9a03179043a3120b50aff9d6b417d75b
Trong Nghia <trongnghia77@live.com>
2024-05-21 18:42:02 +0700

    07-django-template-if-tag

--- myapp01/appone/views.py
+++ myapp01/appone/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, "appone/index.html")
+    data = {'name': 'Nghia'}
+    return render(request, "appone/index.html", data)
 
 def register(request):
     return render(request, "appone/register.html")
