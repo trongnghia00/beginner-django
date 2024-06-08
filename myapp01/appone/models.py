5fda284fa7fa478b4a3f3ef18b7b58fb9987bbd9
Trong Nghia <trongnghia77@live.com>
2024-06-08 07:58:47 +0700

    09-model-task

--- myapp01/appone/models.py
+++ myapp01/appone/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
