a5df78b1cb52db005ea2b409addf1d0fbafc6be7
Trong Nghia <trongnghia77@live.com>
2024-06-21 09:56:31 +0700

    11-foreign-key

--- myapp01/appone/models.py
+++ myapp01/appone/models.py
@@ -6,3 +6,8 @@ class Task(models.Model):
     description = models.TextField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
 
+class Review(models.Model):
+    reviewer_name = models.CharField(max_length=70)
+    review_title = models.CharField(max_length=100)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
