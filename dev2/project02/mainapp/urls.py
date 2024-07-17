9e1629c79b17e3232ae40ddc2d3c03c877b6b070
Trong Nghia <trongnghia77@live.com>
2024-07-17 10:19:56 +0700

    24-new-app-and-create-views

---
+++ dev2/project02/mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name=''),
+    path('register', views.register, name='register'),
+    path('login', views.my_login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
---
+++ dev2/project02/mainapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def homepage(request):
+    pass
+
+def register(request):
+    pass
+
+def my_login(request):
+    pass
+
+def dashboard(request):
+    pass
