a7f97432a28abf0731d74fab29fde58f2ff4fd0e
Trong Nghia <trongnghia77@live.com>
2024-08-02 10:28:05 +0700

    33-create-thought-model

--- dev2/project02/mainapp/models.py
+++ dev2/project02/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Thought(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.CharField(max_length=400)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, max_length=10, on_delete=models.CASCADE)
